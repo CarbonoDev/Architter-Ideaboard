82a6fd251e2268a5cdc324d8cda91cdbf89f5662
Marco Rivadeneyra <rockiano@gmail.com>
2012-10-16 20:58:09 -0500

    Agregados fragmentos y Actionbar
    Signed-off-by: Marco Rivadeneyra <rockiano@gmail.com>

---
+++ src/com/example/architter/HomeFragment.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.example.architter;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Marco
+ *
+ */
+public class HomeFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.home_fragment, container, false);
+	}
+}
