2aa1679dceafa9f7c239a1af084f4a8528fcafcd
Marco Rivadeneyra <rockiano@gmail.com>
2012-11-02 15:49:56 -0600

    Avance de arch  this
    Signed-off-by: Marco Rivadeneyra <rockiano@gmail.com>

--- src/com/example/architter/HomeFragment.java
+++ src/com/example/architter/HomeFragment.java
@@ -25,17 +25,6 @@ public class HomeFragment extends Fragment {
 	    Context darkTheme = new ContextThemeWrapper(getActivity(), R.style.AppTheme);
 	    inflater = (LayoutInflater) darkTheme.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View homeView = inflater.inflate(R.layout.home_fragment, container, false);
-		LinearLayout column1 = (LinearLayout) getActivity().findViewById(R.id.linear1);
-		TextView tx = new TextView(getActivity());
-		tx.setText("hola!");
-		column1.addView(tx);
-		tx = new TextView(getActivity());
-		tx.setText("hola2");
-		column1.addView(tx);
-		IdeaWidget idea = new IdeaWidget(getActivity(), null);
-		idea.setDescription("Descripcion1");
-		idea.setImage("http://ycombinator.com/images/yc500.gif");
-		column1.addView(idea);
 		return homeView;
 	}
 }
