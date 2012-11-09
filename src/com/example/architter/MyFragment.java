ea9a4d28bee2b4660bf411f4dc6aedd81acc09a1
Marco Rivadeneyra <rockiano@gmail.com>
2012-11-09 14:56:59 -0600

    Cargar fragment en view
    Signed-off-by: Marco Rivadeneyra <rockiano@gmail.com>

--- src/com/example/architter/MyFragment.java
+++ src/com/example/architter/MyFragment.java
@@ -6,13 +6,17 @@ import android.app.FragmentTransaction;
 public class MyFragment extends Fragment {
 
 	public void loadFragment(Fragment newFragment, Fragment previous) {
+		loadFragment(newFragment, previous, android.R.id.content);
+	}
+
+	public void loadFragment(Fragment newFragment, Fragment previous, int view) {
 		// Create new fragment and transaction
 		FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
 		// Replace whatever is in the fragment_container view with this fragment,
 		// and add the transaction to the back stack
 		transaction.hide(previous);
-		transaction.replace(android.R.id.content, newFragment);
+		transaction.replace(view, newFragment);
 		transaction.addToBackStack(null);
 
 		// Commit the transaction
