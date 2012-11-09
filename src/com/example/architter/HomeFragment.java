b48de5ce82f9c74e5e013764d3e396dbb309d4a9
Jose Martinez <mtz.josecarlos@gmail.com>
2012-11-09 13:01:50 -0600

    TopContaniner Estilos y Eventos
    TopContainerButtons cambian el estilo cuando se seleccionan

--- src/com/example/architter/HomeFragment.java
+++ src/com/example/architter/HomeFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 /**
@@ -15,6 +16,8 @@ import android.widget.LinearLayout;
  *
  */
 public class HomeFragment extends MyFragment implements OnClickListener {
+	
+	Button arquitectureButton, interiorButton, furnitureButton, setsButton;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +25,14 @@ public class HomeFragment extends MyFragment implements OnClickListener {
 		View homeView = inflater.inflate(R.layout.home_fragment, container, false);
 		LinearLayout column1 = (LinearLayout) homeView.findViewById(R.id.linear2);
 		LinearLayout column2 = (LinearLayout) homeView.findViewById(R.id.linear3);
+		arquitectureButton = (Button) homeView.findViewById(R.id.arquitectureButton);
+		interiorButton = (Button) homeView.findViewById(R.id.interiorButton);
+		furnitureButton = (Button) homeView.findViewById(R.id.furnitureButton);
+		setsButton = (Button) homeView.findViewById(R.id.setsButton);
+		arquitectureButton.setOnClickListener(this);
+		interiorButton.setOnClickListener(this);
+		furnitureButton.setOnClickListener(this);
+		setsButton.setOnClickListener(this);
 		IdeaWidget idea;
 		for(int i = 0; i < 20; i++){
 			idea = (IdeaWidget) inflater.inflate(R.layout.idea_component, column1, false);
@@ -50,16 +61,54 @@ public class HomeFragment extends MyFragment implements OnClickListener {
 	}
 	
 	public void onClick(View v) {
-		IdeaWidget i = (IdeaWidget) v.getParent();
-		int id = i.getIdeaId();
-		if(v.getId() == R.id.image) {
-			IdeaViewFragment newFragment = new IdeaViewFragment();
-			newFragment.setIdea_id(id);
-			loadFragment(newFragment, this);
-		} else if (v.getId() == R.id.archthis) {
-			ArchThisFragment newFragment = new ArchThisFragment();
-			newFragment.setIdea_id(id);
-			loadFragment(newFragment, this);
+		switch(v.getId()) {
+			case R.id.arquitectureButton: {
+				interiorButton.setSelected(false);
+				furnitureButton.setSelected(false);
+				setsButton.setSelected(false);
+				v.setSelected(true);
+				break;
+			}
+			case R.id.interiorButton: {
+				arquitectureButton.setSelected(false);
+				furnitureButton.setSelected(false);
+				setsButton.setSelected(false);
+				v.setSelected(true);
+				break;
+			}
+			case R.id.furnitureButton: {
+				arquitectureButton.setSelected(false);
+				interiorButton.setSelected(false);
+				setsButton.setSelected(false);
+				v.setSelected(true);
+				break;
+			}
+			case R.id.setsButton: {
+				arquitectureButton.setSelected(false);
+				interiorButton.setSelected(false);
+				furnitureButton.setSelected(false);
+				v.setSelected(true);
+				break;
+			}
+			case R.id.image: {
+				IdeaWidget i = (IdeaWidget) v.getParent();
+				int id = i.getIdeaId();
+				IdeaViewFragment newFragment = new IdeaViewFragment();
+				newFragment.setIdea_id(id);
+				loadFragment(newFragment, this);
+				break;
+			}
+			case R.id.archthis: {
+				IdeaWidget i = (IdeaWidget) v.getParent();
+				int id = i.getIdeaId();
+				ArchThisFragment newFragment = new ArchThisFragment();
+				newFragment.setIdea_id(id);
+				loadFragment(newFragment, this);
+				break;
+			}
+			default: {
+				break;
+			}
 		}
 	 }
 
